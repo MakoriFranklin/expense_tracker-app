70d163698cdec03cd824953dadbcdaaa7be05b68
Franklin Makori <franklinmakori795@gmail.com>
2024-03-13 22:21:21 +0300

    Frank first Commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+    <h2 className='header'>
+        Expense tracker
+    </h2>
+      
+    </>
+  )
+}
+
+export default Header
